5cc13574c460641f38f2273a314ba4a9a0606bc1
stackconner <connerstack@gmail.com>
2016-08-19 08:37:38 -0500

    variable exercise questions 1-6
    answers to the exercise questions for variables. #7 is on it's own
    repository do to error syncing to github.

---
+++ variableexercise/Source.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	
+	//EXAMPLE 1
+	//int answer = 2/3 + 3/5;
+
+	//printf("%i,answer");
+	//system("pause");
+		//return 0;
+
+	//EXAMPLE 2
+//	int answer = 2 % 3 + 3 % 5;
+//
+//	printf("%i", answer);
+//	system("pause");
+//	return 0;
+
+	//EXAMPLE 3
+	//int answer = 23 % 15 % -2;
+	//printf("%i", answer);
+	//system("pause");
+	//return 0;
+
+	//EXAMPLE 4
+
+	/*int answer = 25 * 1 / 2;
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 5;
+	/*int answer = 25 * 1.0 / 2;
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 6
+	/*int answer = 25 * (1 / 2);
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 7
+	/*int answer = 235 / 8 + 7;
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 8
+	/*int answer = 235 / 8.0 + 7;
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 9
+	/*int answer = ((20 + 1) / 2 - 2.0) / (23 + 3)*0.2;
+	printf("%i", answer);
+	system("pause");
+	return 0;*/
+
+	//EXAMPLE 10
+	//int avgheight
+	
+	//EXAMPLE 11
+	// int IncTaxPercentUp
+
+	//EXAMPLE 12
+	//int IncTaxPercentDown
+
+	//EXAMPLE 13
+	//SteamLibTotal
+	
+	//EXAMPLE 14
+	//int
+
+	//EXAMPLE 15
+	//float
+
+	//EXAMPLE 16
+	//char
+
+	//EXAMPLE 17
+	//double
+
+	//EXAMPLE 18
+	//bool
+
+	//EXAMPLE 19
+	/*int numa = 10;
+	int numb= 25;
+	int tmp;
+
+	printf("select 2 numbers \n");
+	
+	tmp = numb;
+	numb = numa;
+	numa = tmp;
+	
+	cout << numa << numb;
+
+
+
+	
+	system("pause");
+	return 0;*/
+
+//EXAMPLE 20
+//int i = 0;
+//float f = 0;
+//char ch = 'a';
+//cin >> i >> ch >> f;
+//cout << "i:" <<i << "\nch:" << ch << "\nf:" << f << endl;
+//system("pause");
+//return 0;
+
+//A: i: 1 ch: A f: 45
+//B: i: 1 ch: A f: 45
+//C: i: 1 ch: 9 f: 45
+//D: i: 1 ch: 9 f: 45
+//E: i:0 ch: a f: 0
+//F: i:1 ch: B f:0
+
+//EXAMPLE 21
+//int a = 0;
+//int b = 0;
+//int c = 0;
+//int d = 0;
+//int e = 0;
+//int total = 5;
+//int ans;
+//printf("input 5 numbers to average \n");
+//cin >> a >> b >> c >> d >> e;
+//ans = (a + b + c + d + e) / total;
+//cout<< ans<< endl;
+//system("pause");
+//return 0;
+
+
+char A;
+char B;
+char C;
+
+cin >> A >> B;
+
+
+}
+
